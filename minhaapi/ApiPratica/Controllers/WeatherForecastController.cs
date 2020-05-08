4f4c91cc89fa9942c93950b460ea53e45860628c
Cesar Augusto <c.augusto@btp.com.br>
2020-05-08 10:21:04 -0300

    Add exemplo

---
+++ minhaapi/ApiPratica/Controllers/WeatherForecastController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPratica.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Oiiiiii");
+        }
+    }
+}
